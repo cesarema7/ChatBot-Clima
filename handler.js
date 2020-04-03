f78928881123efa51737bf81ec0bb9cc97f1ab37
cesarema7 <36248828+cesarema7@users.noreply.github.com>
2020-04-02 21:02:12 -0600

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,29 @@
+'use strict';
+const axios = require("axios");
+
+module.exports.getWeather = async (event) => {
+  const ciudad = event.currentIntent.slots["Ciudad"];
+  const url = "http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&units=metric&APPID=3c4c52694850c00cef05dd5111e9c111";
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    const answer = "La temperatura es " + data.main.temp + "° C y la humedad es " 
+                    + data.main.humidity + "% y " + data.weather[0].description + " se espera.";
+    
+    return {
+      "sessionAttributes": {},
+      "dialogAction": {
+        "type": "Close",
+        "fulfillmentState": "Fulfilled",
+        "message": {
+          "contentType": "PlainText",
+          "content": answer
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
